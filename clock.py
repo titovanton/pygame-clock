4e04fe1951806ed78764cfa35c5abbb329b58f84
Titov Anton <webdev@titovanton.com>
2021-09-12 01:44:43 +0500

    calc points works

--- clock.py
+++ clock.py
@@ -1,11 +1,12 @@
 import math
 from decimal import Decimal
+from itertools import cycle
 
 from collections import namedtuple
 
 import pygame as pg
 
-from colors import WHITE
+from colors import WHITE, BLUE, GREEN, RED
 
 
 CLOCK_COLOR = WHITE
@@ -13,9 +14,9 @@ Point = namedtuple('Point', ['x', 'y'])
 
 
 class Clock:
-    padding = Decimal(10)
+    padding = 10
     division_rect = (5, 25)
-    angle = Decimal(0)
+    angle = 0
 
     def __init__(self, surface):
         self.surface = surface
@@ -25,19 +26,23 @@ class Clock:
         print(self.radius)
         self.middle = Point(width / 2, height / 2)
         self.start_point = Point(width / 2, self.padding)
+        self._color_generator = cycle([WHITE, GREEN, RED, BLUE, RED, WHITE, BLUE, GREEN])
+
+    def _get_color(self):
+        return self._color_generator.__next__()
 
     def _draw(self, point):
-        pg.draw.circle(self.surface, CLOCK_COLOR, (float(point.x), float(point.y)), 5)
+        pg.draw.circle(self.surface, self._get_color(), (float(point.x), float(point.y)), 5)
 
     def _calc_point(self):
         x1 = self.start_point.x
         y1 = self.start_point.y
-        x2 = x1 + Decimal(math.sin(self.angle)) * self.radius
-        y2 = y1 + self.radius * Decimal(1 - math.cos(self.angle))
+        x2 = math.ceil(x1 + Decimal(math.sin(self.angle)) * self.radius)
+        y2 = math.ceil(y1 + self.radius * Decimal(1 - math.cos(self.angle)))
         return Point(x2, y2)
 
     def _inc_angle(self):
-        self.angle = self.angle - Decimal(360 / 60)
+        self.angle = self.angle + Decimal(math.pi / 30)
 
     def update(self):
         point = self._calc_point()
